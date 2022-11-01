9705b276f74439cbf06c6a64d9dab408ce6c9c1a
Divyaharika Rayudu <109732178+divyaharika2@users.noreply.github.com>
2022-11-01 18:39:33 +0530

    n=int(input()) arr=list(map(int,input().split())) p=set() s=0 for i in range(n):     if arr[i]%2!=0:         p.add(arr[i]) for i in p:     s+=i print(s)

---
+++ unique_odd_elements_sum.py
@@ -0,0 +1,13 @@
+n=int(input())
+arr=list(map(int,input().split()))
+p=set()
+s=0
+for i in range(n):
+    if arr[i]%2!=0:
+        p.add(arr[i])
+for i in p:
+    s+=i
+print(s)    
+    
+        
+        
